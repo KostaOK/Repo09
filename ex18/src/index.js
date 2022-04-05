6dc1c3de83ae885e9366d0d5f93fa28f0229a291
Niko Kostič <kostic.niko@icloud.com>
2022-04-05 23:36:54 +0200

    For Loops

---
+++ ex18/src/index.js
@@ -0,0 +1,27 @@
+function myForLoop1() {
+    var evenNumbers = "";
+    for (var i = 0; i <= 0; i=i+2) {
+        if (i==0) {
+            evenNumbers=evenNumbers+i;
+        } else {
+            evenNumbers=evenNumbers + i + ",";
+        }
+    } return evenNumbers;
+
+}function myForLoop2() {
+    var evenNumbers = "";
+    for (var i = 8; i >= 0; i=i-2) {
+        if (i==0) {
+            evenNumbers=evenNumbers+i;
+        } else {
+            evenNumbers=evenNumbers + i + ",";
+        }
+    } return evenNumbers;
+}
+console.log(myForLoop1());
+console.log(myForLoop2());
+
+module.exports = {
+    myForLoop1,
+    myForLoop2
+}
